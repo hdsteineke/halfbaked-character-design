6799ef97381d7b5c92c2302d01505d0574c1c2a8
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-09 15:57:52 -0800

    successfully tracking and display number of times outfits are changed

--- app.js
+++ app.js
@@ -81,10 +81,14 @@ catchphraseButton.addEventListener('click', () => {
 
 function displayStats() {
     // text content of the reportEl to tell the user how many times they've changed each piece of the state
+    reportEl.textContent = `You have changed the top ${headCount} times, the middle ${midCount} times, and the bottom ${bottomCount} times.`
+
 }
 
 function displayCatchphrases() {
     // clear out the DOM for the currently displayed catchphrases
+    catchphrasesEl.textContent = '';
+
 
     // loop through each catchphrase in state
    
