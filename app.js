2ebcfc386027e10b0bf021e7c073e3bbc7f537a4
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-09 15:26:08 -0800

    successfully clearing the input value after clicking button

--- app.js
+++ app.js
@@ -71,10 +71,9 @@ catchphraseButton.addEventListener('click', () => {
     
     // push the new catchphrase to the catchphrase array in state
     catchPhrases.push(newPhrase);
-    console.log(catchPhrases);
     
-
     // clear out the form input's value so it's empty to the user
+    catchphraseInput.value = '';
    
     // update the dom to show the new catchphrases (refactor to/call displayCatchphrases to do this work)
 
