d42d3b1703b5ea64a5134b9423ad7700e9932d16
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-09 14:44:35 -0800

    successfully able to change top, middle, and bottom images on page, and incremented changes for each

--- app.js
+++ app.js
@@ -29,32 +29,40 @@ headDropdown.addEventListener('change', () => {
     // update the dom for the head (use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
     headEl.style.backgroundImage = `url("./assets/${headType}-head.png")`;
     
-
-    
-
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
+
 });
 
 
 middleDropdown.addEventListener('change', () => {
     // get the value of the middle dropdown
+    const midType = middleDropdown.value;
 
     // increment the middle change count state
+    midCount++;
     
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
+    middleEl.style.backgroundImage = `url(./assets/${midType}-middle.png)`;
 
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
+    
 });
 
 
 bottomDropdown.addEventListener('change', () => {
     // get the value of the bottom dropdown
+    const bottomType = bottomDropdown.value;
 
     // increment the bottom change count state
+    bottomCount++;
     
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
+    bottomEl.style.backgroundImage = `url(./assets/${bottomType}-pants.png)`;
 
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 catchphraseButton.addEventListener('click', () => {
